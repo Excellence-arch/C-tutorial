4ae5c045618e670c673aee17bccda29922bb7191
excellence-arch <oladipupomichael9@gmail.com>
2023-01-27 08:28:31 +0300

    basic data types

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define MAX_LIMIT 20
 int main() {
+    //char a[MAX_LIMIT];
+    //scanf("%c", &a);
+    //gets(a);
+    //fgets(a, MAX_LIMIT, stdin);
+    //scanf("%[^\n]%c", a);
+    scanf("%[^\n]s", a);
+    printf("%s", a);
     return 0;
 }
